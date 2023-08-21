c8ccc08cbc6067fbbea0f24047c9b0447bd75e06
M O N O <110067389+MONIUSZKO@users.noreply.github.com>
2023-08-22 01:57:32 +0200

    Create config.lua

---
+++ config/config.lua
@@ -0,0 +1,28 @@
+Config        = {}
+
+Config.Target = 'qtarget'                       -- tutaj ustaw swój target (qtarget lub bt-target)
+
+Config.Ped    = 'a_m_y_breakdance_01'           -- tutaj daj nazwę peda pod którego podpięta będzie sprzedaż (nie zapomnij dodać lokacji w ped_spawner)
+
+Narko         = {
+    ['weed_lemon'] = {
+        min = 10,
+        max = 15
+    },
+    ['opium'] = {
+        min = 40,
+        max = 55,
+    },
+    ['coke'] = {
+        min = 50,
+        max = 75,
+    },
+}
+
+--
+-- W Z Ó R
+--
+--    ['nazwa_narko'] = {
+--        min = najniższa_cena,
+--        max = najwyższa_cena,
+--    },
